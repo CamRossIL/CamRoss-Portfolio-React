b2e239193a9d48358228f5c12ae881d7562f525e
cross <camillrossetti@gmail.com>
2026-04-16 18:28:47 +0200

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Legales from "./pages/Legales";
+
 function App() {
-  
   return (
-    <>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/legales" element={<Legales />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
