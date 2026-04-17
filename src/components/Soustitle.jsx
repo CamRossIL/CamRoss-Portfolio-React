6e67efb74e6dfe196e8ea842caebad832bc0f554
cross <camillrossetti@gmail.com>
2026-04-17 10:21:26 +0200

    soustitle component

---
+++ src/components/Soustitle.jsx
@@ -0,0 +1,10 @@
+function Soustitle({ title }) {
+  return (
+    <h3 className="fw-bold border-bottom border-primary border-5 pb-2">
+      {title}
+    </h3>
+  );
+}
+
+export default Soustitle;
+ 
