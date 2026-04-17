5d876432b4c6702ba423c21e1e0b26315e5fa79e
cross <camillrossetti@gmail.com>
2026-04-17 09:51:04 +0200

    add footer to app

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Legales from "./pages/Legales";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
@@ -17,8 +18,9 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/legales" element={<Legales />} />
       </Routes>
+      <Footer />
     </div>
   );
-} 
+}
 
 export default App;
