5d8dbfa855bf6684381c2dae37522a59566dc3f7
cross <camillrossetti@gmail.com>
2026-04-17 10:35:15 +0200

    creation composant Banniere

---
+++ src/components/Banniere.jsx
@@ -0,0 +1,10 @@
+import Banniereimg from "../assets/images/banner.jpg";
+
+function Banniere() {
+  return (
+    <img src={Banniereimg} alt="banniere" className="img-fluid"/>
+  );
+}
+
+export default Banniere;
+ 
