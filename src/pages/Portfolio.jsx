9cde365bb2b9ffc0d2e9820b42ddc560fe7a121a
cross <camillrossetti@gmail.com>
2026-04-17 10:54:49 +0200

    structure portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,16 @@
+import Banniere from "../components/Banniere";
+import Title from "../components/Title";
+
 function Portfolio() {
   
   return (
-    <>
-    </>
+    <div>
+      <Banniere/>
+      <Title
+        title="Portfolio"
+        text="Voici quelques-unes de mes réalisations."
+      />
+    </div>
   )
 }
 
