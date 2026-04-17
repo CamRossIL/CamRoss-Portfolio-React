410439e155cd46273e3621da05d7a93e8a934b2f
cross <camillrossetti@gmail.com>
2026-04-17 10:04:34 +0200

    structure hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+function Hero() {
+  return (
+    <section>
+      <h1>
+        Bonjour, je suis John Doe
+      </h1>
+      <h2>
+        Développeur web full-stack
+      </h2>
+      <button className="btn btn-danger">
+        En savoir plus
+      </button>
+    </section>
+  );
+}
+
+export default Hero;
