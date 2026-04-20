fd0a98d78aff58e3d4d651da0b98d4cd7d562fa9
cross <camillrossetti@gmail.com>
2026-04-20 15:37:47 +0200

    google maps

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -123,6 +123,14 @@ function Contact() {
                     <span> john.doe@gmail.com</span>
                   </p>
                 </div>
+                <div className="ratio ratio-16x9">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2782.626965754038!2d4.796403975777966!3d45.77866571240285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb65edac5b3f%3A0xe01c47049cb2e2b9!2s40%20Rue%20Laure%20Diebold%2C%2069009%20Lyon!5e0!3m2!1sfr!2sfr!4v1775637719813!5m2!1sfr!2sfr"                    
+                    allowFullScreen
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                  ></iframe>
+                </div>
               </div>
             </div>
           </div>
