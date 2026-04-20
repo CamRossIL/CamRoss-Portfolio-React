4c4118e9b92bc2df4c23f913d5d3bdd39d1d4548
cross <camillrossetti@gmail.com>
2026-04-20 15:15:34 +0200

    structure contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import Title from "../components/Title";
+import Soustitle from "../components/Soustitle";
 
 function Contact() {
   return (
@@ -8,9 +9,23 @@ function Contact() {
         text="Pour me contacter en vue d'un entretien ou d'une future collaboration, 
       merci de remplir le formulaire de contact."
       />
+      <div className="container my-5 p-3">
+        <div className="card">
+          <div className="card-body">
+            <div className="row">
+              <div className="col-12 col-md-6">
+                <Soustitle title="Formulaire de contact" />
+                {/* formulaire de contact */}
+              </div>
+              <div className="col-12 col-md-6">
+                <Soustitle title="Mes coordonnées" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Contact;
- 
