84a0b46affe1edfebce9d109e87b33227ae2d974
cross <camillrossetti@gmail.com>
2026-04-20 15:07:20 +0200

    add title to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,16 @@
+import Title from "../components/Title";
+
 function Contact() {
-  
   return (
-    <>
-    </>
-  )
+    <div>
+      <Title
+        title="Contact"
+        text="Pour me contacter en vue d'un entretien ou d'une future collaboration, 
+      merci de remplir le formulaire de contact."
+      />
+    </div>
+  );
 }
 
 export default Contact;
+ 
