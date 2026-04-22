b934737fe9e198f0c0928a8518627e198ecdb2db
cross <camillrossetti@gmail.com>
2026-04-22 12:38:13 +0200

    function closeModal and link to button btn-close

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,54 @@ import SectionHome from "../components/SectionHome";
 import { useState } from "react";
 
 function Home() {
- /*créer state affichage modal*/
+  /*créer state affichage modal*/
   const [showModal, setShowModal] = useState(false);
 
-/*créer fonctions ouvrir modal */
+  /*créer fonctions ouvrir modal */
   function openModal() {
     setShowModal(true);
   }
- 
+
+  function closeModal(){
+    setShowModal(false)
+  }
 
   return (
     <div>
       <Hero openModal={openModal} />
-      <SectionHome/>
-      {showModal && <p>Modal</p> }
+      <SectionHome />
+      {showModal && (
+        <div className="modal show d-block" tabIndex="-1">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title"></h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={closeModal}
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <p>Modal body text goes here.</p>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
