08f14006b142de8ce21f2000c976f295af0f69b1
cross <camillrossetti@gmail.com>
2026-04-22 12:20:03 +0200

    state affichage modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import Hero from "../components/Hero";
 import SectionHome from "../components/SectionHome";
+import { useState } from "react";
 
 function Home() {
+ /*créer state affichage modal*/
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div>
       <Hero />
