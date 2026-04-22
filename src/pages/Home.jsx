00417da4f6be10042be29d39722641744afadd93
cross <camillrossetti@gmail.com>
2026-04-22 13:15:10 +0200

    modal content moved to modal component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,9 +18,9 @@ function Home() {
   initialement il n'y a aucune donnée */
   const [user, setUser] = useState(null);
 
-   /*créer fonction pour aller chercher et stocker les données, puis executér fonction */ 
-/*utiliser useEffect pour que donnees une seule fois au chargement */
- useEffect(() => {
+  /*créer fonction pour aller chercher et stocker les données, puis executér fonction */
+  /*utiliser useEffect pour que donnees une seule fois au chargement */
+  useEffect(() => {
     const getUser = async () => {
       const res = await fetch("https://api.github.com/users/github-john-doe");
       const json = await res.json();
@@ -29,77 +29,11 @@ function Home() {
     getUser();
   }, []);
 
-  
-
   return (
     <div>
       <Hero openModal={openModal} />
       <SectionHome />
-      {showModal && (
-        <div className="modal show d-block" tabIndex="-1">
-          <div className="modal-dialog modal-lg">
-            <div className="modal-content bg-dark text-white">
-              <div className="modal-header">
-                <h5 className="modal-title">Mon profil GitHub</h5>
-                <button
-                  type="button"
-                  className="btn-close btn-close-white"
-                  onClick={closeModal}
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div className="modal-body">
-                <div className="row">
-                   <div className="col-12 col-md-6">
-                {user && (
-                  <img
-                    src={user.avatar_url}
-                    alt={user.login}
-                    className="img-fluid"
-                  />
-                )}
-              </div>
-                  <div className="col-12 col-md-6">
-                <p className="border-bottom">
-                  <i className="bi bi-person me-2"></i>
-                  {user && (
-                    <a href={user.html_url} target="_blank" rel="noreferrer">
-                      {user.name}
-                    </a>
-                  )}
-                </p>
-                <p className="border-bottom ">
-                  <i className="bi bi-geo-alt me-2"></i>
-                  <span>{user && user.location}</span>
-                </p>
-                <p className="border-bottom ">
-                  <i className="bi bi-card-text me-2"></i>
-                  {user && user.bio}
-                </p>
-                <p className="border-bottom ">
-                  <i className="bi bi-box me-2"></i>
-                  <span>Repositories : {user && user.public_repos}</span>
-                </p>
-                <p className="border-bottom ">
-                  <i className="bi bi-people me-2"></i>
-                  <span>Followers : {user && user.followers}</span>
-                </p>
-                <p>
-                  <i className="bi bi-people me-2"></i>
-                  <span>Following : {user && user.following}</span>
-                </p>
-              </div>
-                </div>
-              </div>
-              <div className="modal-footer">
-                <button className="btn btn-secondary" onClick={closeModal}>
-                  Fermer
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+      {showModal && <Modal user={user} closeModal={closeModal} />}
     </div>
   );
 }
